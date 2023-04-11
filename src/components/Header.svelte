8e67369e727800aaffc1ce26f5dba31879a4ba85
msk <15199219+muskit@users.noreply.github.com>
2023-04-10 23:40:48 -0700

    initial commit

---
+++ src/components/Header.svelte
@@ -0,0 +1,35 @@
+<script>
+
+</script>
+
+<div class="container vertical-center">
+    <div style="flex:1;background-color:red">
+        
+    </div>
+    <div style="flex:1; font-size: 72px;">
+        <a style="color: #1B4486">web</a><a style="color: #387cea">PDA</a>
+    </div>
+    <div style="flex:1;background-color:blue">
+
+    </div>
+</div>
+
+<style>
+    .container {
+        display:flex;
+        justify-content:center;
+        align-items:center;
+
+        width:100%;
+        height:96px;
+
+        border-style: solid;
+        border-width: 2px;
+    }
+
+    .vertical-center {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
